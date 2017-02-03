d1025db2aa886d4ef1490f540587c521e2caa42b
Radha Krishnan Vinayagam <vradha@vt.edu>
2017-02-03 16:58:00 -0500

    Updated dashboard
    - Added filters
    - Enhanced graphs
    - Removed data due to privacy concerns

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-d3.csv("hhc_pibb.csv", function(data){
+var college = "COE", term = null;
+$(document).ready(function(){
+     draw();
+     $(".radio-inline").on("click", function(){
+       term = $(this).find('input').attr('id'); 
+       draw();
+    });
     
-    var dateFormat = d3.time.format('%Y');
-    var numberFormat = d3.format('.2f');
+    $("select").change(function(){
+       college = $(this).val();
+       draw();
+    });
+    
+    //$("#chart").controlsUseVisibility(true);
+});
+
+function draw(){
+    d3.csv("hhc_pibb.csv", function(data){
+    
 
     data.forEach(function (d) {
         d.Year = new Date(d.Year,0,1);
@@ -15,6 +30,8 @@ d3.csv("hhc_pibb.csv", function(data){
     var enrByDeptChart = dc.pieChart('#dc-enr-by-dept');
     var totalEnrDeptChart = dc.lineChart('#dc-total-enr-dept');
     
+  
+    
     var facts = crossfilter(data);
     var all = facts.groupAll();
 
@@ -25,7 +42,7 @@ d3.csv("hhc_pibb.csv", function(data){
         
     var n = all.reduceCount().value();
     console.log(n);
-    /*
+    
     var yearGroup = yearDim.group().reduce(
         function(p,v){
             ++p.count;
@@ -49,40 +66,50 @@ d3.csv("hhc_pibb.csv", function(data){
             };
         }        
     );
-    */
-    collDim.filter("COE");
-    //termDim.filter("Fall");
+    
+    collDim.filter(college);
+    termDim.filter(term);
+   /* $(".radio-inline").on("click", function(){
+       collDim.filter(($(this).find('input').attr('id'))); 
+    });*/
+    
    
     var yearValGroup = yearDim.group()
            .reduceSum(function(d){ return d.headcount_pibb; });
    
     totalEnrChart.width(990)
-            .height(250)
+            .height(350)
             .transitionDuration(1500)
             .dimension(yearDim)
             .group(yearValGroup)
-            .colorAccessor(function(d){  return d.value; })
+            .margins({top:10, right: 50, bottom: 30, left: 50})
             .keyAccessor(function(d){ return d.key; })
+            .valueAccessor(function(d){ return d.value; })
             .radiusValueAccessor(function(d){ return d.value/10; })
             .maxBubbleRelativeSize(0.3)
             .x(d3.time.scale().domain([new Date(2012, 0, 1), new Date(2016, 0, 1)]))
-            .y(d3.scale.linear().domain([1000, 70000]))
-            .r(d3.scale.linear().domain([0, 70000]))
-            .elasticY(false)
+            .y(d3.scale.linear().domain([1000, 20000]))
+            .r(d3.scale.linear().domain([100, 30000]))
+            .yAxisLabel("PIBB Headcount")
+            .elasticY(true)
             .elasticX(true)
             .renderLabel(false)
             .yAxisPadding(100)
             .xAxisPadding(500);
-            
+   
+   //totalEnrChart.yAxis().tickFormat(function(d){debugger;return d3.format(',d')(d);});
     console.log(yearValGroup);
     
     totalEnrDeptChart.width(480)
             .height(350)
             .transitionDuration(1500)
+            .margins({top:10, right: 50, bottom: 30, left: 50})
             .dimension(yearDim)
             .group(yearValGroup)
             .elasticY(true)
             .x(d3.time.scale().domain([new Date(2011, 0, 1), new Date(2016, 0, 1)]))
+            .y(d3.scale.linear().domain([1000, 20000]))
+            .yAxisLabel("PIBB Headcount")
             .yAxisPadding(100)
             .xAxisPadding(500)
             .xAxis();
@@ -99,4 +126,4 @@ d3.csv("hhc_pibb.csv", function(data){
     dc.renderAll();
     
 });
-
+}
